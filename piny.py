5c9a8ec7083d4286bb7ee968b4ebe56232b12093
demono10000 <pawel@soltys.com.pl>
2022-04-21 01:54:49 +0200

    Stworzyłem plik do zarządzania pinami

---
+++ piny.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO
+import elementyGlobalne
+
+
+def ustawPiny():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(elementyGlobalne.piny['kraniecSilnik1Gora'], GPIO.IN, GPIO.PUD_UP)
+    GPIO.setup(elementyGlobalne.piny['kraniecSilnik1dol'], GPIO.IN, GPIO.PUD_UP)
+    GPIO.setup(elementyGlobalne.piny['kraniecSilnik2Gora'], GPIO.IN, GPIO.PUD_UP)
+    GPIO.setup(elementyGlobalne.piny['kraniecSilnik2dol'], GPIO.IN, GPIO.PUD_UP)
+    GPIO.setup(elementyGlobalne.piny['wykrytoKolizje'], GPIO.IN, GPIO.PUD_UP)
