0a777e069ada44ab52fc0c86bc36666684433cd5
hanwha <ps@bmo.pl>
2022-07-21 09:22:05 +0200

    Dodano brakujące piny

--- elementyGlobalne.py
+++ elementyGlobalne.py
@@ -4,7 +4,15 @@ piny = {'kraniecSilnik1Gora': 13,
         'kraniecSilnik1dol': 16,
         'kraniecSilnik2Gora': 26,
         'kraniecSilnik2dol': 21,
-        'wykrytoKolizje': 18}
+        'wykrytoKolizje': 18,
+        'enkoder1': 20,
+        'enkoder2': 23,
+        'czujnikIR1': 24,
+        'czujnikIR2': 25,
+        'silnik1stan': 27,
+        'silnik2stan': 5,
+        'silnik1kierunek': 22,
+        'silnik2kierunek': 6}
 pomin = [0, 10, 87, 77, 38, 49]
 wybranaSoczewka = None
 soczewka = -1
